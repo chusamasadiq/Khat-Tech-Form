1924e00ee6e90853bbb605775c36afc89297af61
chusamasadiq <usamasadiq163@gmail.com>
2022-10-21 11:57:58 +0500

    Khat-Tech Form

--- assets/js/JQuery.js
+++ assets/js/JQuery.js
@@ -31,21 +31,32 @@ $(document).ready(function () {
         });
     });
 
+    var row = $('#row-container .row:eq(0)').clone();
+    $('#addButton').data('row', row);
+    $('#addButton').click(function () {
+        $('#row-container').append($(this).data('row').clone());
+    });
+    $('#removeButton').click(function () {
+        $('#row-container .row').eq($('#row-container .row').length - 1).remove();
+    });
+
+
+
     // Experience Details Input Field 
     $(document).ready(function () {
         var max_fields = 10; //maximum input boxes allowed
         var wrapper = $(".input_fields_experience"); //Fields wrapper
-        var add_button = $(".add_field_experience"); //Add button ID
+        var add_button_exp = $(".add_field_experience"); //Add button ID
 
         var x = 1; //initlal text box count
-        $(add_button).click(function (e) {
+        $(add_button_exp).click(function (e) {
             //on add input button click
             e.preventDefault();
             if (x < max_fields) {
                 //max input box allowed
                 x++; //text box increment
                 $(wrapper).append(
-                    '<div class="input-group mb-3"><input type="text" name="experience[]" class="form-control"/> <a href="#" class="remove_field input-group-append"><span class="input-group-text"><i class="fa fa-times" aria-hidden="true"></i></span></a> </div>'
+                    '<div class="input-group mb-5"><input type="text" name="experience[]" class="form-control"/> <a href="#" class="remove_field input-group-append"><span class="input-group-text"><i class="fa fa-times" aria-hidden="true"></i></span></a> </div>'
                 ); //add input box
             }
         });
@@ -57,4 +68,42 @@ $(document).ready(function () {
             x--;
         });
     });
+
+    var row = $('#row-containerExp .row:eq(0)').clone();
+    $('#addExpButton').data('row', row);
+    $('#addExpButton').click(function () {
+        $('#row-containerExp').append($(this).data('row').clone());
+    });
+    $('#removeExpButton').click(function () {
+        $('#row-containerExp .row').eq($('#row-containerExp .row').length - 1).remove();
+    });
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    window.onload = function () {
+        //Reference the DropDownList.
+        var ddlYears = document.getElementById("startYearEdu");
+ 
+        //Determine the Current Year.
+        var currentYear = (new Date()).getFullYear();
+ 
+        //Loop and add the Year values to DropDownList.
+        for (var i = 1950; i <= currentYear; i++) {
+            var option = document.createElement("OPTION");
+            option.innerHTML = i;
+            option.value = i;
+            ddlYears.appendChild(option);
+        }
+    };
+
+    
 })
